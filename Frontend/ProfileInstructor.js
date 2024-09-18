5fe05b706c3a00d176858f21fdcef1d3329c560e
Nevi63 <cdpinkus@gmail.com>
2024-09-17 18:25:12 -0600

    Perfil instructor
    la version de "mi perfil" que se mostrara cuando un instructor entre  la seccion mi perfil

--- Frontend/ProfileInstructor.js
+++ Frontend/ProfileInstructor.js
@@ -41,14 +41,12 @@ document.addEventListener('DOMContentLoaded', function () {
     // Function to load courses
     function loadCourses() {
         const courses = [
-            { img:'https://miro.medium.com/v2/resize:fit:698/1*0jjdu52m0MO4SjLWiCVOlg.jpeg',title: 'Como ser cantante profesional', instructor: 'Hatsune Miku', link: 'CourseDetail.html' },
-            { img:'https://bs-uploads.toptal.io/blackfish-uploads/components/open_graph_image/8959179/og_image/optimized/0712-Bad_Practices_in_Database_Design_-_Are_You_Making_These_Mistakes_Dan_Social-754bc73011e057dc76e55a44a954e0c3.png',title: 'Como hacer una base de datos mamastrosa', instructor: 'Villatrue', link: 'CourseDetail.html' },
-            { img:'https://miro.medium.com/v2/resize:fit:698/1*0jjdu52m0MO4SjLWiCVOlg.jpeg',title: 'Como ser cantante profesional', instructor: 'Hatsune Miku', link: 'CourseDetail.html' },
-            { img:'https://bs-uploads.toptal.io/blackfish-uploads/components/open_graph_image/8959179/og_image/optimized/0712-Bad_Practices_in_Database_Design_-_Are_You_Making_These_Mistakes_Dan_Social-754bc73011e057dc76e55a44a954e0c3.png',title: 'Como hacer una base de datos mamastrosa', instructor: 'Villatrue', link: 'CourseDetail.html' },
-         ];
+            { img:'https://miro.medium.com/v2/resize:fit:698/1*0jjdu52m0MO4SjLWiCVOlg.jpeg',title: 'Como ser cantante profesional', link: 'CourseDetail.html' },
+            { img:'https://bs-uploads.toptal.io/blackfish-uploads/components/open_graph_image/8959179/og_image/optimized/0712-Bad_Practices_in_Database_Design_-_Are_You_Making_These_Mistakes_Dan_Social-754bc73011e057dc76e55a44a954e0c3.png',title: 'Como hacer una base de datos mamastrosa',  link: 'CourseDetail.html' },
+           ];
 
         const coursesRow = document.querySelector('#coursesContent .row');
-        coursesRow.innerHTML = ''; // Clear existing content
+        coursesRow.innerHTML = '';
 
         courses.forEach(course => {
             const card = document.createElement('div');
@@ -56,9 +54,9 @@ document.addEventListener('DOMContentLoaded', function () {
             card.innerHTML = `
                 <div class="card">
                     <div class="card-body">
+                        <button type="button" class="btn btn-danger btn-sm position-absolute top-0 end-0 m-2">Eliminar</button>
                         <img src="${course.img}" alt="Curso" class="img-fluid course-image">
                         <h5 class="card-title">${course.title}</h5>
-                        <p class="card-text">${course.instructor}</p>
                         <a href="${course.link}" class="btn btn-primary">Ver Curso</a>
                     </div>
                 </div>
