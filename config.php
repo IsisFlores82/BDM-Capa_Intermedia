8100c03b3d842922965955d0452fd92cee19f18d
Nevi63 <cdpinkus@gmail.com>
2024-09-30 15:04:21 -0600

    comienzo movimiento dashboard
    empezar a mudar todo a php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    "database" => [
+        "host" => "localhost",
+        "user" => "root",
+        "password" => "",
+        "dbname" => "pwci_c1",
+        "port" => "3306",
+    ],
+];
