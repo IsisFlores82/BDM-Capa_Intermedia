fac8883b93dffb60263d6b07f77ce595693fbfc6
Nevi63 <cdpinkus@gmail.com>
2024-11-24 02:14:14 -0600

    gitignore

--- BDM-CI/config.php
+++ BDM-CI/config.php
@@ -4,10 +4,18 @@ return [
 
    "database" => [
      "host" => "localhost",
-     "user" => "root",
-     "password" => "",
-     "dbname" => "BDMCAPA",
+     "user" => "mikuacad_pinkus",
+     "password" => "Abc1234.Abc1234.",
+     "dbname" => "mikuacad_BDMCAPA",
      "port" => "3306",
    ],
+   "facebook"=>[
+       "id"=>"1057657179704580",
+       "version"=>"v21.0",
+       ],
+    "paypal"=>[
+        "client_id"=>"AeS_Q2aLUv1dulWB4h3r3uyOS-cjgn8Lrs0WbMAE8kL20c9OvIUDNF99iMwkXFg7hKiBXhxdUt5YIMMJ",
+        "cliente_secret"=>"EFlG_KjkgIbeoYqFHabYKVA-oP9k3w8zR6g7GmnYfozy1FQE--25j7Hl8lj_iz82x2nBhgeTbzXCtyZB",
+        ],
    //aqui se podrian enlistar apis 
 ];
